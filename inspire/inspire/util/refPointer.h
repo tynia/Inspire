4b5738d977a32508dbd827e3b6af9b5ddc90070f
tynia <xduilib@gmail.com>
2015-07-24 09:57:25 +0800

    refPointer like smart_ptr

---
+++ inspire/inspire/util/refPointer.h
@@ -0,0 +1,89 @@
+#ifndef _INSPIRE_UTIL_REFERENCE_POINTER_H_
+#define _INSPIRE_UTIL_REFERENCE_POINTER_H_
+
+namespace inspire {
+
+   class refCounter
+   {
+   public:
+   protected:
+      refCounter() : _referCount(0)
+      {}
+
+      refCounter(const refCounter* rhs) : _referCount(rhs->_referCount)
+      {}
+
+      ~refCounter()
+      {
+
+      }
+
+      void _inc()
+      {
+         ++_referCount;
+      }
+
+      void _dec()
+      {
+         --_referCount;
+      }
+
+      unsigned int _referCount;
+   };
+
+   template<class T>
+   class refPointer
+   {
+      refPointer(T* ptr) : _ptr(ptr)
+      {
+         _ptrCounter = new refCounter();
+         _ptrCounter->_inc();
+      }
+
+      refPointer(const refPointer& rhs) : _ptr(rhs._ptr), _ptrCounter(rhs._ptrCounter)
+      {
+         _ptrCounter->_inc();
+      }
+
+      ~refPointer()
+      {
+         _ptrCounter->_dec();
+         if (0 == _ptrCounter->_referCount)
+         {
+            delete _ptr;
+            _ptr = NULL;
+
+            delete _ptrCounter;
+            _ptrCounter = NULL;
+         }
+      }
+
+      refPointer<T>& operator= (const refPointer<T>& rhs)
+      {
+         if (_ptr == rhs._ptr)
+         {
+            return *this;
+         }
+
+         _ptrCounter->_dec();
+         _ptr = rhs._ptr;
+         _ptrCounter = rhs._ptrCounter;
+         _ptrCounter->_inc();
+      }
+
+      T& operator* ()
+      {
+         return *_ptr;
+      }
+
+      T* operator-> ()
+      {
+         return _ptr;
+      }
+
+   private:
+      T* _ptr;
+      refCounter* _ptrCounter;
+   };
+}
+#endif
