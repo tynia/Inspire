76afe2d5272502bd6600be19747ebd7463749807
tynia <xduilib@gmail.com>
2015-12-21 19:58:31 +0800

    remove useless file

--- sample/sample_ObjectMgr.cpp
+++ sample/sample_ObjectMgr.cpp
@@ -11,11 +11,28 @@ public:
    void say() { std::cout << "A Object says" << std::endl; }
 };
 
+class BObject : public inspire::iObject
+{
+public:
+   BObject() { std::cout << "B Object constructor" << std::endl; }
+   ~BObject() { std::cout << "B Object destructor" << std::endl; }
+
+   void say() { _num = 100; std::cout << "B Object says" << _num << std::endl; }
+
+private:
+   int _num;
+};
+
 int main(int argc, char* argv)
 {
-   AObject* obj = INSPIRE_NEW AObject;
+   AObject* obj = INSPIRE_NEW AObject();
    INSPIRE_ASSERT(NULL != obj, "obj cannot be NULL");
    obj->say();
    INSPIRE_DEL obj;
+
+   BObject a;
+   BObject *pB = INSPIRE_NEW BObject();
+   a.say();
+   pB->say();
    return 0;
 }
